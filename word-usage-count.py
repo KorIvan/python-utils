136a09d64bd1bef39147598fafdbf05b6b9b164b
Ivan <kornelyuk.ivan.andre@gmail.com>
2023-11-27 20:09:27 +0800

    comparators for words

--- word-usage-count.py
+++ word-usage-count.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import pathlib
+from functools import cmp_to_key
 
 filename = "filename"
 parser = argparse.ArgumentParser(
@@ -11,6 +12,27 @@ parser.add_argument("-m", "--most-used-first", action='store_true', help="Most u
 parser.add_argument("-s", "--short-first", action='store_true', help="Shortest words will be printed first")
 parser.add_argument("-a", "--alphabet", action='store_true', help="Print words in alphabetical order")
 parser.add_argument("filename", help="Full path to the essay text file")
+
+
+def alphabet_comparator(x, y):
+    if x > y:
+        return 1
+    elif x < y:
+        return -1
+    else:
+        return 0
+
+
+def len_comparator(x, y):
+    wordX = x[0]
+    wordY = y[0]
+    if len(wordX) > len(wordY):
+        return 1
+    elif len(wordX) < len(wordY):
+        return -1
+    else:
+        return alphabet_comparator(wordX, wordY)
+
 
 def printPretty(dictionary):
     print("{:<8} {:<15}".format('Count', 'Word'))
@@ -35,8 +57,8 @@ with open(essay) as file:
                 dic[lc] = 1
 
 if (arguments.short_first):
-    printPretty(dict(sorted(dic.items(), key= lambda word: len(word[0]))))
+    printPretty(dict(sorted(dic.items(), key=cmp_to_key(len_comparator))))
 if (arguments.most_used_first):
-    print(dic.items())
     printPretty(dict(sorted(dic.items(), key=lambda x: x[1], reverse=True)))
-
+if (arguments.alphabet):
+    printPretty(dict(sorted(dic.items(), key=lambda x: x[0])))
